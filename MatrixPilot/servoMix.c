e593baf2d2533e6368959235a894f1ebd8775dd5
Coby Leuschke <coby@metanomy.org>
2011-12-25 13:59:10 -0700

    adding VTOL mixing

--- MatrixPilot/servoMix.c
+++ MatrixPilot/servoMix.c
@@ -174,7 +174,39 @@ void servoMix( void )
 			udb_pwOut[THROTTLE_OUTPUT_CHANNEL] = udb_servo_pulsesat( temp ) ;
 		}
 #endif
+
+	// VTOL airplane airframe
+	// Mix roll_control, pitch_control, into aileron and elevator
+	// Mix roll_control into rudder
+	// Mix rudder_control into  rudder
+#if ( AIRFRAME_TYPE == AIRFRAME_VTOL )
+		long delta_roll_control = REVERSE_IF_NEEDED(ELEVON_VTAIL_SURFACES_REVERSED, roll_control) ;
 		
+		temp = pwManual[AILERON_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(AILERON_CHANNEL_REVERSED, -delta_roll_control + pitch_control) ;
+		udb_pwOut[AILERON_OUTPUT_CHANNEL] = udb_servo_pulsesat( temp ) ;
+		
+		temp = pwManual[ELEVATOR_INPUT_CHANNEL] +
+			REVERSE_IF_NEEDED(ELEVATOR_CHANNEL_REVERSED, delta_roll_control + pitch_control) ;
+		udb_pwOut[ELEVATOR_OUTPUT_CHANNEL] = udb_servo_pulsesat( temp ) ;
+		
+		temp = pwManual[RUDDER_INPUT_CHANNEL] + 
+			REVERSE_IF_NEEDED(RUDDER_CHANNEL_REVERSED, yaw_control) ;
+		udb_pwOut[RUDDER_OUTPUT_CHANNEL] =  udb_servo_pulsesat( temp ) ;
+		
+		if ( pwManual[THROTTLE_INPUT_CHANNEL] == 0 )
+		{
+			udb_pwOut[THROTTLE_OUTPUT_CHANNEL] = 0 ;
+		}
+		else
+		{	
+			temp = pwManual[THROTTLE_INPUT_CHANNEL] + REVERSE_IF_NEEDED(THROTTLE_CHANNEL_REVERSED, throttle_control) ;
+			udb_pwOut[THROTTLE_OUTPUT_CHANNEL] = udb_servo_pulsesat( temp ) ;
+		}
+#endif
+
+
+
 		udb_pwOut[PASSTHROUGH_A_OUTPUT_CHANNEL] = udb_servo_pulsesat( pwManual[PASSTHROUGH_A_INPUT_CHANNEL] ) ;
 		udb_pwOut[PASSTHROUGH_B_OUTPUT_CHANNEL] = udb_servo_pulsesat( pwManual[PASSTHROUGH_B_INPUT_CHANNEL] ) ;
 		udb_pwOut[PASSTHROUGH_C_OUTPUT_CHANNEL] = udb_servo_pulsesat( pwManual[PASSTHROUGH_C_INPUT_CHANNEL] ) ;
